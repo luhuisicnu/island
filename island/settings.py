1a87cdda1ed522c3cd7c088d9a0ee2c3941c37a8
hank <luhuisicnu@163.com>
2018-01-11 15:24:16 +0800

    问答模块model设计

--- island/settings.py
+++ island/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'djcelery',
     'customize_auth',
+    'qa',
 ]
 
 MIDDLEWARE = [
---
+++ qa/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from customize_auth.models import User
+
+
+class Question(models.Model):
+    subject = models.CharField(max_length=255, unique=True, verbose_name='提问标题')
+    description = models.TextField(verbose_name='提问描述')
+    type = models.CharField(max_length=40, verbose_name='问题类型')
+    solved = models.BooleanField(default=False, verbose_name='解决状态')
+    disabled = models.BooleanField(default=False, verbose_name='禁用')
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, related_name='questions', on_delete=models.CASCADE)
+    first_answer = models.OneToOneField('Answer', related_name='acception_as_first',
+                                        null=True, on_delete=models.SET_NULL)
+    best_answer = models.OneToOneField('Answer', related_name='acception_as_best',
+                                       null=True, on_delete=models.SET_NULL)
+
+
+class Answer(models.Model):
+    content = models.TextField(verbose_name='回答内容')
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    disabled = models.BooleanField(default=False, verbose_name='禁用')
+    question = models.ForeignKey('Question', related_name='answers', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='answers', on_delete=models.CASCADE)
+    approvals = models.ManyToManyField(User, related_name='approval_answer')
+    negations = models.ManyToManyField(User, related_name='negation_answer')
+
+    @property
+    def acception(self):
+        if hasattr(self, 'acception_as_first') or hasattr(self, 'acception_as_best'):
+            return True
+        return False
